be6be12e8d1801e1020d844fff1761975534a4cc
Sabrina <s2br2n2@gmail.com>
2023-07-27 13:34:07 +0100

    Initial Commit

---
+++ Sabrina/SabrinaApp.swift
@@ -0,0 +1,17 @@
+//
+//  SabrinaApp.swift
+//  Sabrina
+//
+//  Created by scholar on 27/07/2023.
+//
+
+import SwiftUI
+
+@main
+struct SabrinaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
