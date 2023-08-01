cce4da2daf50888bcae16ca0e2ca5b62b10e8840
Sabrina <s2br2n2@gmail.com>
2023-08-01 09:30:51 +0100

    Added comment

--- Sabrina/ContentView.swift
+++ Sabrina/ContentView.swift
@@ -6,14 +6,33 @@
 //
 
 import SwiftUI
-
+//COMMENTTTTT
 struct ContentView: View {
+    
+    @State private var colour = ""
+    @State private var txtTitle = "Guess Sabrina's favourite colour!"
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundColor(.accentColor)
-            Text("Hello, world!")
+        VStack (spacing: 79){
+            Text(txtTitle)
+                .font(.title)
+                .fontWeight(.heavy)
+                .foregroundColor(Color.blue)
+            TextField("Type here!", text: $colour)
+                .multilineTextAlignment(.center)
+                .font(.title)
+                .border(Color.gray,width: 0.5)
+                .padding(.all)
+            Button("Submit") {
+                if colour == "Blue"{
+                    txtTitle = "Correct!"
+                 
+                }
+                else {
+                    txtTitle = "You don't know me very well..."
+                }
+                    
+            }
         }
         .padding()
     }
